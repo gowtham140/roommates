2cd8a35569b5a0e99117370cafa97030fe092e10
gowtham140 <gowthamramesh1406@gmail.com>
2026-09-24 21:46:56 +0530

    Initial commit

---
+++ roommates_BE/src/main/java/com/roomates/RoommatesBeApplication.java
@@ -0,0 +1,13 @@
+package com.roomates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommatesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoommatesBeApplication.class, args);
+	}
+
+}
